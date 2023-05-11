da1dcd8c4eecc293f55754e9e3583e5b8014509e
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-05-11 13:17:24 -0600

    PR Cleanup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -80,7 +80,7 @@ app.use('/graphql', graphqlHTTP(() => ({
 // })
 
 const server = app.listen(5001, '0.0.0.0', () => {
-
+  console.log('App listening at http://0.0.0.0:5001') //eslint-disable-line
   const wsServer = new WebSocketServer({ server, path: '/graphql' })
   useServer({ schema }, wsServer)
 })
