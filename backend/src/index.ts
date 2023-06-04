180913a75310c0ebd65390f8a05e78e687c49f1b
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-06-04 15:44:36 -0600

    Added Sentry

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,40 +1,54 @@
 import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
 import { useServer } from 'graphql-ws/lib/use/ws'
+import * as Sentry from '@sentry/node'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import { WebSocketServer } from 'ws'
+
 import schema from './schemas'
 import { logger } from './utilities'
 import { handleSpotifyUserRedirect } from './spotify'
+
 const app = express()
+Sentry.init({
+  dsn: 'https://838c24cda4bd47d09cfbe44a11406585@o196886.ingest.sentry.io/4505303436886016',
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    // Automatically instrument Node.js libraries and frameworks
+    ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+  ],
 
-// process.on('uncaughtException', (error: any) => logger(error))
-// process.on('unhandledRejection', (error: any) => logger(error))
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
 
-// app.use(Sentry.Handlers.requestHandler())
-// app.use(Sentry.Handlers.tracingHandler())
+app.use(Sentry.Handlers.requestHandler())
+app.use(Sentry.Handlers.tracingHandler())
 
-const CORS_DEV = [
-  'localhost:3001',
-]
+const CORS_DEV = ['localhost:3001']
 
-const COORS_PROD = [
-  'https://playlists.sillysideprojects.com/'
-]
+const COORS_PROD = ['https://playlists.sillysideprojects.com/']
 
 // For Cors
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
   next()
 })
-app.use(cors({
-  origin: process.env.NODE_ENV === 'production'
-    ? COORS_PROD
-    : CORS_DEV
-
-}))
+app.use(
+  cors({
+    origin: process.env.NODE_ENV === 'production' ? COORS_PROD : CORS_DEV,
+  })
+)
 
 app.use(bodyParser.json())
 
@@ -42,41 +56,35 @@ app.get('/ok', async (req: express.Request, res: express.Response) => {
   res.send('pong!')
 })
 
-app.get('/spotify_redirect', async (req: express.Request, res: express.Response) => {
-  const response = await handleSpotifyUserRedirect(req.query)
-  if (response === null) {
-    res.sendStatus(500)
-    return
-  }
-  res.redirect(response)
-})
-
-app.use('/graphql', graphqlHTTP(() => ({
-  schema,
-  graphiql: process.env.NODE_ENV !== 'production',
-  customFormatErrorFn: (err: any) => {
-    logger(err.message)
-    // if (err.message in errorLookup) return errorLookup[err.message]
-    return {
-      statusCode: 500,
-      message: 'Something went wrong'
+app.get(
+  '/spotify_redirect',
+  async (req: express.Request, res: express.Response) => {
+    const response = await handleSpotifyUserRedirect(req.query)
+    if (response === null) {
+      res.sendStatus(500)
+      return
     }
+    res.redirect(response)
   }
-})))
+)
 
-// app.use(Sentry.Handlers.errorHandler())
-// app.use((err, req: express.Request, res: express.Response) => {
-//   res.statusCode = 500
-// })
+app.use(
+  '/graphql',
+  graphqlHTTP(() => ({
+    schema,
+    graphiql: process.env.NODE_ENV !== 'production',
+    customFormatErrorFn: (err: any) => {
+      logger(err.message)
+      // if (err.message in errorLookup) return errorLookup[err.message]
+      return {
+        statusCode: 500,
+        message: 'Something went wrong',
+      }
+    },
+  }))
+)
 
-// Sentry.init({
-//   dsn: 'https://f0f907615c134aff90c1a7d1ea17eb34@o4504279410671616.ingest.sentry.io/4504279411851264',
-//   integrations: [
-//     new Sentry.Integrations.Http({ tracing: true }),
-//     new Tracing.Integrations.Express({ app }),
-//   ],
-//   tracesSampleRate: 1.0,
-// })
+app.use(Sentry.Handlers.errorHandler())
 
 const server = app.listen(8080, '0.0.0.0', () => {
   console.log('App listening at http://0.0.0.0:8080') //eslint-disable-line
@@ -85,4 +93,4 @@ const server = app.listen(8080, '0.0.0.0', () => {
   useServer({ schema }, wsServer)
 })
 
-export { }
+export {}
