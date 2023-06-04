c6df50e6a0601cf2b228530c4211bc139b5d9aca
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-06-04 15:24:08 -0600

    Added feedback form

--- frontend/src/algorithms.tsx
+++ frontend/src/algorithms.tsx
@@ -1,4 +1,8 @@
-import { FromArtistToArtist, ProgressivelyEnergetic, GoodBeatsToGoodSleeps } from './pages/algorithms'
+import {
+  FromArtistToArtist,
+  ProgressivelyEnergetic,
+  GoodBeatsToGoodSleeps,
+} from './pages/algorithms'
 
 interface TAlgorithm {
   title: string
@@ -9,21 +13,30 @@ interface TAlgorithm {
 
 export const ALGORITHM_ROUTES: TAlgorithm[] = [
   {
-    title: 'Progressively Energetic',
-    href: '/a/progressively_energetic',
-    description: 'Pick an artist. Generate a playlist of songs related to that artist. The songs will start mellow and end with lots of energy.',
-    component: (title, description) => <ProgressivelyEnergetic title={title} description={description} />
-  },
-  {
     title: 'From Artist to Artist',
     href: '/a/from_artist_to_artist',
-    description: 'Pick two artists. Generate a playlist of songs that starts with one and ends with the other.',
-    component: (title, description) => <FromArtistToArtist title={title} description={description} />
+    description:
+      'Pick two artists. Generate a playlist of songs that starts with one and ends with the other.',
+    component: (title, description) => (
+      <FromArtistToArtist title={title} description={description} />
+    ),
   },
   {
     title: 'Good Beats to Good Sleeps',
     href: '/a/good_beats_to_good_sleeps',
-    description: 'Pick an arist. Generate a playlist of songs that starts with the selected artist and ends with enough white noise to last you through the night.',
-    component: (title, description) => <GoodBeatsToGoodSleeps title={title} description={description} />
-  }
+    description:
+      'Pick an arist. Generate a playlist of songs that starts with the selected artist and ends with enough white noise to last you through the night.',
+    component: (title, description) => (
+      <GoodBeatsToGoodSleeps title={title} description={description} />
+    ),
+  },
+  {
+    title: 'Progressively Energetic',
+    href: '/a/progressively_energetic',
+    description:
+      'Pick an artist. Generate a playlist of songs related to that artist. The songs will start mellow and end with lots of energy.',
+    component: (title, description) => (
+      <ProgressivelyEnergetic title={title} description={description} />
+    ),
+  },
 ]
