5f68b04b2f2bc8c0f1e06e28f3867401851489be
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-06-16 21:44:59 -0600

    Progress towards first rewrite

--- frontend/src/pages/algorithms/AlgorithmWrapper.tsx
+++ frontend/src/pages/algorithms/AlgorithmWrapper.tsx
@@ -1,14 +1,73 @@
-import { Container, Typography } from '@mui/material'
+import { Button, Container, Typography } from '@mui/material'
+import { Loading, Playlist } from 'sharedComponents'
+import { useCallback, useContext, useMemo, useState } from 'react'
 
 import { pageWrapperCSS } from 'theme'
+import { type TPlaylistEntry } from 'playlist-generator-utilities'
+import { context } from 'context'
 
-const AlgorithmWrapper = ({ title, description, children }: { title: string, description: string, children: any }) => {
+interface AlgorithmWrapperProps {
+  title: string
+  description: string
+  children: any
+  searchParams?: JSX.Element
+  searchDisabled?: boolean
+  isSearching?: boolean
+  apiCall: () => Promise<TPlaylistEntry[] | undefined>
+  resetStateCallback: () => void
+}
+
+enum EStep {
+  Inputting = 'Inputting',
+  Searching = 'Searching',
+  NoResults = 'NoResults',
+  PreviewingPlaylist = 'PreviewingPlaylist',
+}
+
+const AlgorithmWrapper = ({ title, description, children, searchParams, searchDisabled, apiCall, resetStateCallback }: AlgorithmWrapperProps) => {
+  const [playlistEntries, setPlaylistEntries] = useState<TPlaylistEntry[]>([])
+  const [step, setStep] = useState<EStep>(EStep.Inputting)
+  const { state, dispatch } = useContext(context)
+
+  const resetState = useCallback(() => {
+    setPlaylistEntries([])
+    resetStateCallback()
+  }, [resetStateCallback])
+
+  const handleSearch = useCallback(async () => {
+    setStep(EStep.Searching)
+
+    if (!state.user) {
+      dispatch({ type: 'ADD_ALERT', data: { text: 'User is not logged in', severity: 'error' } })
+      return
+    }
+
+    const results = await apiCall()
+
+    setStep(results ? EStep.PreviewingPlaylist : EStep.NoResults)
+  }, [dispatch, state.user, apiCall])
+
+  const Content = useMemo(() => {
+    switch (step) {
+      case EStep.Inputting:
+        return searchParams
+      case EStep.NoResults:
+        return <Typography>No results found</Typography>
+      case EStep.PreviewingPlaylist:
+        return <Playlist resetStateCallback={resetState} initialTitle={'BRB'} playlistEntries={playlistEntries} />
+      case EStep.Searching:
+        return <Loading />
+    }
+  }, [step, searchParams, playlistEntries, resetState])
+
   return (
     <Container css={pageWrapperCSS}>
       <Typography variant="h2" gutterBottom>{title}</Typography>
       <Typography variant="body1" gutterBottom>{description}</Typography>
       <Container>
-        {children}
+        {searchParams}
+        <Button fullWidth variant='contained' disabled={searchDisabled} onClick={handleSearch}>Search</Button>
+        {Content}
       </Container>
     </Container >
   )
--- frontend/src/pages/algorithms/FromArtistToArtist.tsx
+++ frontend/src/pages/algorithms/FromArtistToArtist.tsx
@@ -1,9 +1,8 @@
 import { gql, useLazyQuery } from '@apollo/client'
-import { Button, Typography } from '@mui/material'
-import { useCallback, useContext, useMemo, useState } from 'react'
+import { useCallback, useContext, useState } from 'react'
 
-import { Search, Playlist, Loading } from 'sharedComponents'
-import { type TFromArtistToArtist, type TAutocompleteEntry, type TPlaylistEntry } from 'playlist-generator-utilities'
+import { Search } from 'sharedComponents'
+import { type TFromArtistToArtist, type TAutocompleteEntry } from 'playlist-generator-utilities'
 import { context } from 'context'
 import AlgorithmWrapper from './AlgorithmWrapper'
 
@@ -29,22 +28,15 @@ query createFromArtistToArtistPlaylist($artistIdStart: String!, $artistIdEnd: St
 
 interface FromArtistToArtistParams { title: string, description: string }
 const FromArtistToArtist = ({ title, description }: FromArtistToArtistParams) => {
-  const { state, dispatch } = useContext(context)
+  const { state } = useContext(context)
   const [selectedArtistStart, setSelectedArtistStart] = useState<{ id: string, name: string } | null>(null)
   const [selectedArtistEnd, setSelectedArtistEnd] = useState<{ id: string, name: string } | null>(null)
   const [createFromArtistToArtist] = useLazyQuery<{ createFromArtistToArtistPlaylist: TFromArtistToArtist['Response'] }, TFromArtistToArtist['Request']>(CREATE_FROM_ARTIST_TO_ARTIST_PLAYLIST, { fetchPolicy: 'network-only' })
-  const [playlistEntries, setPlaylistEntries] = useState<TPlaylistEntry[] | null>(null)
-  const [isLoading, setIsLoading] = useState(false)
 
   const resetState = useCallback(() => {
     setSelectedArtistStart(null)
     setSelectedArtistEnd(null)
-    setPlaylistEntries(null)
   }, [])
-
-  const resetStateCallback = useCallback(() => {
-    resetState()
-  }, [resetState])
 
   const resultSelectedCallbackStart = useCallback(async (value: TAutocompleteEntry) => {
     setSelectedArtistStart(value)
@@ -54,59 +46,28 @@ const FromArtistToArtist = ({ title, description }: FromArtistToArtistParams) =>
     setSelectedArtistEnd(value)
   }, [])
 
-  const handleSubmit = useCallback(async () => {
-    setIsLoading(true)
-    if (!selectedArtistStart || !selectedArtistEnd) return
-
-    if (!state.user) {
-      dispatch({ type: 'ADD_ALERT', data: { text: 'User is not logged in', severity: 'error' } })
-      return
-    }
-
-    const result = await createFromArtistToArtist({ variables: { artistIdStart: selectedArtistStart.id, artistIdEnd: selectedArtistEnd.id, market: state.user.market } })
+  const apiCall = useCallback(async () => {
+    const result = await createFromArtistToArtist({ variables: { artistIdStart: selectedArtistStart!.id, artistIdEnd: selectedArtistEnd!.id, market: state.user!.market } })
     if ((result.data?.createFromArtistToArtistPlaylist) != null) {
-      setPlaylistEntries(result.data?.createFromArtistToArtistPlaylist)
+      return result.data?.createFromArtistToArtistPlaylist
     }
+  }, [selectedArtistStart, selectedArtistEnd, createFromArtistToArtist, state.user])
 
-    setIsLoading(false)
-  }, [selectedArtistStart, selectedArtistEnd, createFromArtistToArtist, dispatch, state.user])
-
-  const content = useMemo(() => {
-    if (selectedArtistStart === null || selectedArtistEnd === null || playlistEntries === null) {
-      return (
+  return (
+    <AlgorithmWrapper
+      title={title}
+      description={description}
+      searchParams={
         <>
           <Search label={'Starting Artist'} resultSelectedCallback={resultSelectedCallbackStart} />
           <Search label={'Ending Artist'} resultSelectedCallback={resultSelectedCallbackEnd} />
-          <Button fullWidth variant='contained' disabled={selectedArtistStart === null && selectedArtistEnd === null} onClick={handleSubmit}>Submit</Button>
         </>
-      )
-    }
-
-    if (isLoading) {
-      return (
-        <>
-          <Loading />
-        </>
-      )
-    }
-
-    if (playlistEntries && playlistEntries.length === 0) {
-      return (
-        <Typography>
-          No results found
-        </Typography>
-      )
-    }
-
-    return (
-      <Playlist resetStateCallback={resetStateCallback} initialTitle={`From ${selectedArtistStart.name} to ${selectedArtistEnd.name}`} playlistEntries={playlistEntries} />
-    )
-  }, [playlistEntries, handleSubmit, resultSelectedCallbackStart, resultSelectedCallbackEnd, selectedArtistStart, selectedArtistEnd, resetStateCallback, isLoading])
-
-  return (
-    <AlgorithmWrapper title={title} description={description}>
-      {content}
-    </AlgorithmWrapper>
+      }
+      searchDisabled={selectedArtistStart === null || selectedArtistEnd === null}
+      apiCall={apiCall}
+      resetStateCallback={resetState}
+    >
+    </AlgorithmWrapper >
   )
 }
 
